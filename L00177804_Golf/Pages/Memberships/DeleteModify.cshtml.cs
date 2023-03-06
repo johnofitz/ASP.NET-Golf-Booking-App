d7060ff6d2b50746a2f0d5dfe52301337856e176
johnofitz <johnfitz865@gmail.com>
2023-03-06 20:06:49 +0000

    Added Razor Pages for Deletemodify

---
+++ L00177804_Golf/Pages/Memberships/DeleteModify.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using L00177804_Golf.Data;
+using L00177804_Golf.Model;
+
+namespace L00177804_Golf.Pages.Memberships
+{
+    public class DeleteModifyModel : PageModel
+    {
+        private readonly L00177804_Golf.Data.L00177804_GolfContext _context;
+
+        public DeleteModifyModel(L00177804_Golf.Data.L00177804_GolfContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Membership> Membership { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Membership != null)
+            {
+                Membership = await _context.Membership.ToListAsync();
+            }
+        }
+    }
+}
